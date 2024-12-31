4f634c9e39cfbc25a5b8514886d36691e8e915da
Nikholas Pcenicni <82239765+nikpcenicni@users.noreply.github.com>
2024-12-31 18:59:28 -0500

    .

--- src/pages/LaunchPage.jsx
+++ src/pages/LaunchPage.jsx
@@ -8,7 +8,7 @@ export const LaunchPage = () => {
   const navigate = useNavigate();
 
   const handleLaunch = () => {
-    window.location.href = 'https://youtube.com/redirect?q=https://tesla.pcenicni.ca/apps';
+    window.location.href = 'https://youtube.com/redirect?q=https://tesla.pcenicni.ca/';
   };
 
   const handlePreview = () => {
